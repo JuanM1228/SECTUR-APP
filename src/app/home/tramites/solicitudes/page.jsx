5bfe90c9f1de859e03ec402972101b4c3108dc13
Carlos Méndez <carlos.mdez@outlook.com>
2023-12-10 15:55:39 -0600

    Update documents component

--- src/app/home/tramites/solicitudes/page.jsx
+++ src/app/home/tramites/solicitudes/page.jsx
@@ -14,58 +14,6 @@ import DetalleGenerico from '@/components/solicitudes/formulariosPST/DetalleGene
 import ArrendadoraAutos from '@/components/solicitudes/formulariosPST/ArrendadoraAutos'
 import AlimentosBebidas from '@/components/solicitudes/formulariosPST/AlimentosBebidas'
 
-const documentsList = [
-  {
-    documentId: '232342323',
-    title:
-      'Escritura pública del inmueble, contrato de arrendamiento o contrato de comodato',
-    subtitle:
-      'Documento que acredite la posesión legal o uso del inmueble donde realiza la actividad',
-    fileTypes: '',
-    isRequired: true,
-  },
-  {
-    documentId: '2323423323',
-    title: 'Identificación oficial del promvente',
-    subtitle:
-      'Credencial de elector (INE), cédula profesional, pasaporte del propietario o representante legal',
-    fileTypes: '',
-    isRequired: true,
-  },
-  {
-    documentId: '2322342323',
-    title: 'Comprobante de domicilio',
-    subtitle:
-      'Recibo de agua, luz, teléfono, predial, etc., del domicilio donde se realiza la actividad',
-    fileTypes: '',
-    isRequired: true,
-  },
-  {
-    documentId: '2232342323',
-    title: 'Formato firmado por el propietario o representante legal',
-    subtitle:
-      'Formato único para los trámites del Registro Nacional de Turismo',
-    fileTypes: '',
-    isRequired: true,
-  },
-  {
-    documentId: '1132342323',
-    title: 'Registro Federal de Contribuyentes RFC (Persona Física o Moral)',
-    subtitle:
-      'Documento que acredita la actividad lícita del solicitante de servicios turísticos',
-    fileTypes: '',
-    isRequired: true,
-  },
-  {
-    documentId: '1132342324',
-    title:
-      'Acta Constitutiva (Persona Moral). Persona Física deberá adjuntar RFC',
-    subtitle: 'Documento que acredita la legal constitución de la empresa',
-    fileTypes: '',
-    isRequired: true,
-  },
-]
-
 const Domicilio = dynamic(() => import('@/components/solicitudes/Domicilio'), {
   ssr: false,
 })
@@ -96,7 +44,7 @@ const Solicitudes = () => {
   }
 
   const tipoPST = register.datosGenerales?.tipoPST
-  // const tipoPST = PST_ENUM.HOSPEDAJE
+  // const tipoPST = PST_ENUM.TRANSPORTISTA_TURISTICO
 
   return (
     <div className="h-[calc(100vh-5rem)] flex flex-col justify-start  sm:items-center ">
@@ -136,7 +84,7 @@ const Solicitudes = () => {
       />
       <Documents
         step={step}
-        documentsList={documentsList}
+        pstId={register?.datosGenerales?.tipoPST}
         onSubmitHandler={onSubmitHandler}
       />
       {/* TODO: Añadir subcategoría en los detalles de pst? */}
--- src/components/solicitudes/Documents.jsx
+++ src/components/solicitudes/Documents.jsx
@@ -1,27 +1,46 @@
 'use client'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import Icons from '@/assets/icons'
 import Button from '../common/Button'
 import { STEP_ENUM } from '@/utils/constants'
 
 const Documents = props => {
-  const { documentsList, step, onSubmitHandler } = props
-  // TODO: CHANGE STEP_ENUM value
+  const { step, onSubmitHandler, pstId } = props
+  const [documentsList, setDocumentsList] = useState([])
+
+  useEffect(() => {
+    getDocumentsList()
+  }, [])
+
+  const getDocumentsList = async () => {
+    const url = `/api/registro/cat_docs/${pstId}`
+    try {
+      const res = await sendRequest(url)
+      if (!res.success) return
+      setDocumentsList(res.result.data)
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+
   return (
     <form
       className={`flex flex-col min-w-fit m-4 sm:w-2/3 gap-6 rounded-lg shadow-xl t-ease p-12 ${
         step === STEP_ENUM.DOCUMENTOS ? '' : 'hide'
       }`}
       onSubmit={onSubmitHandler}>
+      <h1 className="font-GMX font-bold text-2xl">DOCUMENTOS</h1>
       {documentsList.map(item => {
-        const acceptFiles = item.fileTypes || 'image/png, image/jpeg, .pdf'
         const isDocumentUploaded = true
         return (
           <div
             key={`i-${item.documentId}`}
             className="border-b border-silver pb-4">
-            <h2 className="font-GMX font-bold">{item.title}</h2>
+            <h2 className="font-GMX font-bold">
+              {item.title}
+              {item.isRequired && <span className="text-error"> *</span>}
+            </h2>
             <h3 className="font-GMX text-gray text-sm">{item.subtitle}</h3>
             <div className="flex items-center mt-2">
               {isDocumentUploaded && (
@@ -29,7 +48,7 @@ const Documents = props => {
               )}
               <input
                 type="file"
-                accept={acceptFiles}
+                accept="image/png, image/jpeg, .pdf"
                 className="block w-full text-sm text-black font-GMX file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-GMX file:bg-bigDipORuby file:text-white file:shadow-xl file:cursor-pointer"
                 onChange={e => handleFileUpload(item.documentId)}
               />
